84e1a4e706b1144638b9beee80d4301f9dec38cb
DESKTOP-HN0EI4G\Nabila <nabilahapriliasalsabila@gmail.com>
2021-10-15 10:59:39 +0700

    deploy

---
+++ resources/views/review.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.admin')
+
+@section('title')
+    <title>Review</title>
+@endsection
+
+@section('content')
+<main class="main">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">Home</li>
+        <li class="breadcrumb-item active">Review</li>
+    </ol>
+    <div class="container-fluid">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">
+                                Daftar Review
+                            </h4>
+                        </div>
+                        <div class="card-body">
+                            @if (session('success'))
+                                <div class="alert alert-success">{{ session('success') }}</div>
+                            @endif
+
+                            @if (session('error'))
+                                <div class="alert alert-danger">{{ session('error') }}</div>
+                            @endif
+
+                           
+                            <div class="table-responsive">
+                                <table class="table table-hover table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama</th>
+                                            <th>Review</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                       
+                                        <?php $no = 0;?>
+                                        @forelse ($feedbacks as $row)
+                                        <?php $no++ ;?>
+                                                    <tr>
+                                                        <td>{{ $no }}</td>
+                                            <td>
+                                            <label><strong>Nama:</strong> {{ $row->username }}</label><br>
+                                                <label><strong>Email:</strong> {{ $row->email }}</label>
+                                            </td>
+                                            <td> {{ $row->review}}</td>
+                                            
+                                           
+                                            <td>
+                                           
+                                                <form action="{{ route('payments.destroy', $row->cart_id) }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                  
+                                                    <button class="btn btn-danger btn-sm">Hapus</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                        @empty
+                                        <tr>
+                                            <td colspan="6" class="text-center">Tidak ada data</td>
+                                        </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                            {!! $feedbacks->links() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
